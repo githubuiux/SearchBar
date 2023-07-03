0a7eae043db24a03bb0a3043e935431543d1570d
githubuiux <uiuxista@gmail.com>
2023-07-03 22:42:22 +0300

    Initial Commit

---
+++ SearchBar/SearchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchBarApp.swift
+//  SearchBar
+//
+//  Created by Levent on 3.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SearchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
